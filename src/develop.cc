81f2e873c19c5fd8077e46d8c794ecb77486a764
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-08-09 13:57:05 +0800

    Modify src develop.cc, extract common util functions out.

--- src/develop.cc
+++ src/develop.cc
@@ -1,20 +1,13 @@
+#include <stdexcept>
 #include <iostream>
-#include <fstream>
-#include <sstream>
 #include <string>
 #include <vector>
-#include <map>
-#include <stdexcept>
 
-#include <stdlib.h>
-#include <math.h>
 #include <argp.h>
-#include <regex.h>
 
-#include "config.h"
-#include "segmenter.h"
 #include "xgboost/c_api.h"
 
+#include "common.h"
 #include "global_dict.h"
 #include "sparse_matrix.h"
 
@@ -108,23 +101,8 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 /* Our argp parser. */
 static struct argp argp = { options, parse_opt, args_doc, prog_doc };
 
-regex_t compile_regex(const char *);
-string regex_search(const regex_t *, int, const string &);
-string regex_replace(const regex_t *, const string &, const string &);
-
-void load_segmenter(const char *, qss::segmenter::Segmenter **);
-void segment(qss::segmenter::Segmenter *, const string &, vector<string> &);
-void normalize(map<string, double> &);
-
-void load_data_file(const char *, GlobalDict &, vector<string> &, SparseMatrix &, vector<int> &);
-DMatrixHandle load_X(SparseMatrix &);
-float *load_y(vector<int> &);
-
 int main(int argc, char *argv[])
 {
-    char *label_file, *template_file, *train_file, *validate_file, *data_file, *netloc_file, *output_file;
-    bool verbose, silent, debug, profile;
-    
     struct arguments arguments;
     arguments.label_file = NULL;
     arguments.template_file = NULL;
@@ -160,7 +138,7 @@ int main(int argc, char *argv[])
     // general parameters
     XGBoosterSetParam(classifier, "booster", "gbtree");
     XGBoosterSetParam(classifier, "silent", "0");
-    XGBoosterSetParam(classifier, "nthread", "12");
+    XGBoosterSetParam(classifier, "nthread", "5");
     
     // booster parameters
     XGBoosterSetParam(classifier, "eta", "0.3");
@@ -183,213 +161,39 @@ int main(int argc, char *argv[])
     XGBoosterSetParam(classifier, "seed", "0");
 
     // train
-    for (int iter = 0; iter < 100; ++iter) {
+    for (int iter = 0; iter < 500; ++iter) {
         cout << "iter: " << iter << endl;
         XGBoosterUpdateOneIter(classifier, iter, X_train);
     }
 
     // validate
-    unsigned long y_pred_length;
-    const float *y_pred;
-    if (XGBoosterPredict(classifier, X_validate, 0, 0, &y_pred_length, &y_pred))
+    unsigned long y_pred_length_train, y_pred_length_validate;
+    const float *y_pred_train, *y_pred_validate;
+    unsigned long counter;
+    
+    if (XGBoosterPredict(classifier, X_train, 0, 0, &y_pred_length_train, &y_pred_train))
         throw runtime_error("error: XGBoosterPredict failed");
-    cout << "y_validate_length: " << matrix_validate.get_nindptr() << "\t" << "y_pred_length: " << y_pred_length << endl;
-    unsigned long counter = 0;
-    for (size_t i = 0; i < y_pred_length; ++i) {
-        cout << "url: " << url_validate[i] << "\t" << "label: " << label_validate[i] << "\t" << "y_pred: " << y_pred[i] << endl;
-        if (label_validate[i] == y_pred[i])
+    counter = 0;
+    for (size_t i = 0; i < y_pred_length_train; ++i)
+        if (label_train[i] == y_pred_train[i])
             counter += 1;
-    }
-    cout << "y_pred_length: "<< y_pred_length << "\t" << "counter: " << "\t" << counter << endl;
+        else
+            cout << "url: " << url_train[i] << "\t" << "label: " << global_dict.label_tag_map[(int) label_train[i]] << "\t" << "y_pred_train: " << global_dict.label_tag_map[(int) y_pred_train[i]] << endl;
+    cout << "acc on training set: " << (double) counter / y_pred_length_train << endl;
+
+    counter = 0;
+    if (XGBoosterPredict(classifier, X_validate, 0, 0, &y_pred_length_validate, &y_pred_validate))
+        throw runtime_error("error: XGBoosterPredict failed");
+    for (size_t i = 0; i < y_pred_length_validate; ++i)
+        if (label_validate[i] == y_pred_validate[i])
+            counter += 1;
+        else
+            cout << "url: " << url_validate[i] << "\t" << "label: " << global_dict.label_tag_map[(int) label_validate[i]] << "\t" << "y_pred_validate: " << global_dict.label_tag_map[(int) y_pred_validate[i]] << endl;
+    cout << "acc on validation set: " << (double) counter / y_pred_length_validate << endl;
+
+    // dump model
+    if (XGBoosterSaveModel(classifier, arguments.model_file))
+        throw runtime_error("error: dumping model failed");
 
     return 0;
-}
-
-DMatrixHandle load_X(SparseMatrix &sparse_matrix)
-{
-    unsigned long *indptr;
-    unsigned int *indices;
-    float *data;
-    sparse_matrix.get_data(&indptr, &indices, &data);
-    
-    unsigned int nindptr = sparse_matrix.get_nindptr();
-    unsigned int nelem = sparse_matrix.get_nelem();
-        
-    DMatrixHandle matrix_handel;
-    int error_code = XGDMatrixCreateFromCSR(indptr, indices, data, nindptr + 1, nelem, &matrix_handel);
-    if (error_code) throw runtime_error("error: XGDMatrixCreateFromCSR failed");
-    return matrix_handel;
-}
-
-float *load_y(vector<int> &label_vec)
-{
-    float *label_array = (float *) malloc(sizeof(float) * label_vec.size());
-    for (size_t i = 0; i < label_vec.size(); ++i) {
-        label_array[i] = (float) label_vec[i];
-    }
-    return label_array;
-}
-
-void load_data_file(const char *data_file, GlobalDict &global_dict, vector<string> &url_vec, SparseMatrix &sparse_matrix, vector<int> &label_vec)
-{
-    ifstream input(data_file);
-    if (!input)
-        throw runtime_error("error: unable to open input file: " + string(data_file));
-
-    regex_t tag_regex = compile_regex("(<LBL>)(.*)(</LBL>)");
-    regex_t title_regex = compile_regex("(<TITLE>)(.*)(</TITLE>)");
-    regex_t url_regex = compile_regex("(<URL>)(.*)(</URL>)");
-    regex_t content_regex = compile_regex("(<CONTENT>)(.*)(</CONTENT>)");
-    regex_t image_regex = compile_regex("\\[img\\][^\\[]*\\[/img\\]");
-    regex_t br_regex = compile_regex("\\[br\\]");
-
-    qss::segmenter::Segmenter *segmenter;
-    load_segmenter("./seg/conf/qsegconf.ini", &segmenter);
-
-    string line;
-    while (getline(input, line)) {
-        string tag = regex_search(&tag_regex, 2, line);
-        string title = regex_search(&title_regex, 2, line);
-        string url = regex_search(&url_regex, 2, line);
-        string content = regex_search(&content_regex, 2, line);
-        content = regex_replace(&image_regex, " ", content);
-        content = regex_replace(&br_regex, " ", content);
-
-        // parse label from tag
-        string::size_type spliter_index = tag.rfind("|");
-        if (spliter_index != string::npos) {
-            tag = string(tag, spliter_index + 1, tag.size());
-        }
-        if (global_dict.sub_parent_map.find(tag) == global_dict.sub_parent_map.end())
-            throw runtime_error(string("error: tag " + tag + " not found in sub_parent_map"));
-        int label = global_dict.tag_label_map[global_dict.sub_parent_map[tag]];
-
-        vector<string> content_seg_vec;
-        vector<string> title_seg_vec;
-        segment(segmenter, content, content_seg_vec);
-
-        // assemble tf
-        int term_count = 0;
-        map<string, int> word_tf_map; // term_count_map
-        for (vector<string>::const_iterator iter = content_seg_vec.begin(); iter != content_seg_vec.end(); ++iter) {
-            map<string, int>::iterator word_tf_iter;
-            if ((word_tf_iter = word_tf_map.find(*iter)) == word_tf_map.end())
-                word_tf_iter = word_tf_map.insert(word_tf_iter, map<string, int>::value_type(*iter, 0));
-            word_tf_iter->second += 1;
-            //word_tf_map[*iter] += 1;
-            term_count += 1;
-        }
-        for (vector<string>::const_iterator iter = title_seg_vec.begin(); iter != title_seg_vec.end(); ++iter) {
-            map<string, int>::iterator word_tf_iter;
-            if ((word_tf_iter = word_tf_map.find(*iter)) == word_tf_map.end())
-                word_tf_iter = word_tf_map.insert(word_tf_iter, map<string, int>::value_type(*iter, 0));
-            word_tf_iter->second += 10;
-            //word_tf_map[*iter] += 10;
-            term_count += 10;
-        }
-
-        // assemble feature (feature -> value)
-        map<string, double> feature_value_map;
-        for (map<string, int>::const_iterator iter = word_tf_map.begin(); iter != word_tf_map.end(); ++iter) {
-            const string &word = iter->first;
-            const double &tf = (double)iter->second / term_count; // the result is equla to not divided by term_count for final feature values
-            map<string, double>::const_iterator word_idf_iter = global_dict.word_idf_map.find(word);
-            if (word_idf_iter != global_dict.word_idf_map.end())
-                feature_value_map[word] = tf * word_idf_iter->second;
-        }
-        normalize(feature_value_map);
-
-        // assemble feature (index -> value)
-        map<int, double> index_value_map;
-        for (map<string, double>::const_iterator feature_value_iter = feature_value_map.begin(); feature_value_iter != feature_value_map.end(); ++feature_value_iter) {
-            map<string, int>::const_iterator word_index_iter = global_dict.word_index_map.find(feature_value_iter->first);
-            if (word_index_iter != global_dict.word_index_map.end())
-                index_value_map[word_index_iter->second] = feature_value_iter->second;
-        }
-
-        url_vec.push_back(url);
-        sparse_matrix.push_back(index_value_map);
-        label_vec.push_back(label);
-    }
-    assert(url_vec.size() == sparse_matrix.get_nindptr());
-    assert(label_vec.size() == sparse_matrix.get_nindptr());
-}
-
-void load_segmenter(const char *conf_file, qss::segmenter::Segmenter **segmenter)
-{
-    qss::segmenter::Config::get_instance()->init(conf_file);
-    *segmenter = qss::segmenter::CreateSegmenter();
-    if (!segmenter)
-        throw runtime_error("error: loading segmenter failed");
-}
-
-void segment(qss::segmenter::Segmenter *segmenter, const string &line, vector<string> &seg_res)
-{
-    int buffer_size = line.size() * 2;
-    char *buffer = (char *) malloc(sizeof(char) * buffer_size);
-    int res_size = segmenter->segmentUtf8(line.c_str(), line.size(), buffer, buffer_size);
-    
-    stringstream ss(string(buffer, res_size));
-    for (string token; ss >> token; seg_res.push_back(token)) ;
-    
-    free(buffer);
-}
-
-void normalize(map<string, double> &feature_value_map)
-{
-    double feature_norm = 0;
-    for (map<string, double>::const_iterator iter = feature_value_map.begin(); iter != feature_value_map.end(); ++iter)
-        feature_norm += iter->second * iter->second;
-    feature_norm = sqrt(feature_norm);
-    for (map<string, double>::iterator iter = feature_value_map.begin(); iter != feature_value_map.end(); ++iter)
-        iter->second /= feature_norm;
-}
-
-regex_t compile_regex(const char *pattern)
-{
-    regex_t regex;
-    int error_code = regcomp(&regex, pattern, REG_EXTENDED);
-    if (error_code != 0) {
-        size_t length = regerror(error_code, &regex, NULL, 0);
-        char *buffer = (char *) malloc(sizeof(char) * length);
-        (void) regerror(error_code, &regex, buffer, length);
-        string error_message = string(buffer);
-        free(buffer);
-        throw runtime_error(string("error: unable to compile regex '") + pattern + "', message: " + error_message);
-    }
-    return regex;
-}
-
-string regex_search(const regex_t *regex, int field, const string &line)
-{
-    regmatch_t match_res[field + 1];
-    int error_code = regexec(regex, line.c_str(), field + 1, match_res, 0);
-    if (error_code != 0) {
-        size_t length = regerror(error_code, regex, NULL, 0);
-        char *buffer = (char *) malloc(sizeof(char) * length);
-        (void) regerror(error_code, regex, buffer, length);
-        string error_message = string(buffer);
-        free(buffer);
-        throw runtime_error(string("error: unable to execute regex, message: ") + error_message);
-    }
-    return string(line, match_res[field].rm_so, match_res[field].rm_eo - match_res[field].rm_so);
-}
-
-string regex_replace(const regex_t *regex, const string &sub_string, const string &ori_string)
-{
-    regmatch_t match_res;
-    int error_code;
-    string res_string = ori_string;
-    while ((error_code = regexec(regex, res_string.c_str(), 1, &match_res, 0)) != REG_NOMATCH) {
-        if (error_code != 0) {
-            size_t length = regerror(error_code, regex, NULL, 0);
-            char *buffer = (char *) malloc(sizeof(char) * length);
-            (void) regerror(error_code, regex, buffer, length);
-            string error_message = string(buffer);
-            free(buffer);
-            throw runtime_error(string("error: unable to execute regex, message: ") + error_message);
-        }
-        res_string = string(res_string, 0, match_res.rm_so) + sub_string + string(res_string, match_res.rm_eo, res_string.size() - match_res.rm_eo);
-    }
-    return res_string;
 }
